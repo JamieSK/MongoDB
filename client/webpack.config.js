b98f631eb35f9fd6017be7373d9b9519e105fd69
Jamie Klein <jamiesklein@gmail.com>
2017-12-12 15:09:59 +0000

    Setup dependencies.

---
+++ client/webpack.config.js
@@ -0,0 +1,10 @@
+let config = {
+  entry: __dirname + '/src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/build',
+  },
+  devtool: 'source-map',
+};
+
+module.exports = config;
